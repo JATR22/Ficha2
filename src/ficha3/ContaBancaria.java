0b4832fe485cd38eba46fae2586900b5613a9290
JATR22 <97111949+JATR22@users.noreply.github.com>
2022-02-04 12:46:49 +0000

    Update

--- src/ficha3/ContaBancaria.java
+++ src/ficha3/ContaBancaria.java
@@ -1,10 +1,35 @@
 package ficha3;
 
+import java.util.Date;
+
 public class ContaBancaria {
-
-	public static void main(String[] args) {
 		
+	private String titular;
+	private double saldo = 0.0;
+	private Date dataAbertura;
+	
+	
+			
+	public ContaBancaria (String titular) {
+		super();
+		this.titular = titular;
+		
+	}
 
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public Date getDataAbertura() {
+		return dataAbertura;
 	}
 
 }
