174f0ea4fcf0a4fb1c1749a14e94b68160eacd62
JATR22 <tr.joaoandre@gmail.com>
2022-03-31 23:06:34 +0100

    ...

---
+++ src/ficha7/src/test/java/com/example/ficha7/Ficha7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ficha7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ficha7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
